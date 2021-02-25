e11d1f359c811ea719e911eb3004631d55dff565
Audrey Diez <audrey.diez@gmail.com>
2021-02-25 19:03:47 +0100

    Feature: contactForm for photographer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./assets/scss/style.scss";
+import { submitEngine } from "./contact_form";
+
 const ENV_prod = false;
+
+
+const contactFormBtn = document.querySelectorAll(".contactForm");
+const backgroundMask = document.getElementById("backgroundMask");
+const contactFormContent = document.getElementById("contactFormModal");
+const closeModalBtn = document.querySelectorAll(".closeModal");
+
+
 
 // If we are in dev mode, display all elements for screen readers
 document.addEventListener("DOMContentLoaded", function() {
@@ -10,3 +20,35 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+
+console.log(contactFormContent);
+// Open modal for contactForm
+contactFormBtn.forEach((btn) => btn.addEventListener("click", openContactForm));
+
+// Close modal for contactForm
+contactFormContent.addEventListener('click', (e) => e.stopPropagation());
+backgroundMask.addEventListener('click',  closeContactForm);
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeContactForm));
+
+//Submit on button
+
+
+
+
+function openContactForm() {
+    backgroundMask.style.display = "flex";
+}
+
+function closeContactForm() {
+    backgroundMask.style.display = "none";
+}
+
+function launchEngine(e) {
+
+    console.log("closing form");
+    e.preventDefault();
+    submitEngine();
+
+}
+
+window.launchEngine = launchEngine;
