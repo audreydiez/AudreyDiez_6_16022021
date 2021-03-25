7afa268387105d3ab4bc0998e9a7caaa09c19cc1
Audrey Diez <audrey.diez@gmail.com>
2021-03-25 16:28:59 +0100

    Updates

--- data-api.js
+++ data-api.js
@@ -1,7 +1,7 @@
 export class DataApi {
   constructor() {
 
-    this.url =  "AudreyDiez_6_16022021/data.json";
+    this.url =  "https://audreydiez.github.io/AudreyDiez_6_16022021/data.json";
   }
 
   async getData() {
